e3ddaea1eca6e09ca3572d0bd3bc95bdf6f938c4
Moh1ts1ngh <thakurmohitsingh2003@gmail.com>
2025-03-05 12:36:17 +0530

    Nothign

--- src/components/ChatBot/ChatBot.tsx
+++ src/components/ChatBot/ChatBot.tsx
@@ -5,7 +5,7 @@ interface ChatBotProps {}
 const ChatBot: React.FC<ChatBotProps> = () => {
   return (
     <div>
-      <h1>This is your chatbot working!</h1>
+      <h1>This is your chatbot working again!</h1>
     </div>
   );
 };
